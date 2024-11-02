c50b9d79b2c319d1871a46d1e38126f33d9efa23
seculoper <mir8725@naver.com>
2024-11-02 22:30:35 +0900

    [Refactor]: 기존 TDD 코드들 삭제

--- src/main/java/com/example/demo/Repository/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Domain.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<Team, Integer> {
-    public Team findTeamByName(String name);
-}
--- src/main/java/com/example/demo/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
