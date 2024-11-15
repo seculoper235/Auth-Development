0ab926b97b434e1972f607fb8fac93aeb1da36f7
seculoper <mir8725@naver.com>
2024-11-15 20:05:52 +0900

    [TEST] JwtProvider 테스트 명시 수정
    * 불필요한 테스트 삭제
    * 테스트의 상황을 명확히 기재

--- src/test/java/com/example/demo/auth/provider/JwtProviderTest.java
+++ src/test/java/com/example/demo/auth/provider/JwtProviderTest.java
@@ -2,48 +2,43 @@ package com.example.demo.auth.provider;
 
 import com.example.demo.config.DomainTestConfig;
 import com.example.demo.domain.common.auth.Token;
+import com.example.demo.domain.common.auth.UserPrincipal;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class JwtProviderTest extends DomainTestConfig {
-    private UserPrincipal userPrincipal;
+    @Autowired
+    private JwtProvider jwtProvider;
 
-    private Token token;
+    private UserPrincipal expectedPrincipal;
+
+    private Token expectedToken;
 
     @BeforeEach
     void setUp() {
-
-        token = new Token("ACCESS_TOKEN", "REFRESH_TOKEN");
+        expectedPrincipal = new UserPrincipal("dev teller", "devteller123@gmail.com");
+        expectedToken = new Token("ACCESS_TOKEN", "REFRESH_TOKEN");
     }
 
     @Test
-    @DisplayName("인증 완료된 사용자 정보를 받으면 토큰 정보를 반환한다")
+    @DisplayName("JWT 생성 시, 인증된 사용자 정보를 받으면 토큰 정보를 반환한다")
     void user_principal_create_token_info_test() {
-        // Given
     }
 
     @Test
-    @DisplayName("검증된 Access Token을 받으면 사용자 정보를 반환한다")
-    void authenticated_access_token_create_user_principal_test() {
-        //
+    @DisplayName("JWT 인증 시, 유효한 JWT를 받으면 사용자 정보를 반환한다")
+    void valid_token_create_user_principal_test() {
     }
 
     @Test
-    @DisplayName("Refresh Token을 받으면 Access Token을 재발급한다")
-    void refresh_token_reissue_test() {
-        //
-    }
-
-    @Test
-    @DisplayName("만료된 JWT를 받으면 TokenExpiredException을 반환한다")
+    @DisplayName("JWT 검증 시, 만료된 JWT를 받으면 TokenExpiredException을 반환한다")
     void expired_token_throw_token_expired_exception() {
-        //
     }
 
     @Test
-    @DisplayName("잘못된 JWT를 받으면 JWTVerificationException을 반환한다")
+    @DisplayName("JWT 검증 시, 잘못된 JWT를 받으면 JWTVerificationException을 반환한다")
     void no_valid_token_throw_jwt_verification_exception() {
-        //
     }
 }
