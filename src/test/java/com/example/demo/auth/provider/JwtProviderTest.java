61c4e93ac173455d3865b7a5b60a38b2d31f8dd3
seculoper <mir8725@naver.com>
2024-11-16 20:03:21 +0900

    [Test] JwtProvider 테스트 수정
    * Test Case Given 작성
    * Test Case Then 명확히 기재

--- src/test/java/com/example/demo/auth/provider/JwtProviderTest.java
+++ src/test/java/com/example/demo/auth/provider/JwtProviderTest.java
@@ -1,43 +1,54 @@
 package com.example.demo.auth.provider;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.demo.config.ComponentTestEnv;
-import com.example.demo.model.common.auth.Token;
 import com.example.demo.model.common.auth.UserPrincipal;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class JwtProviderTest extends ComponentTestEnv {
-    private UserPrincipal expectedPrincipal;
+    private final Long EXPIRATION_MILLISECONDS = 2 * 1000 * 60 * 30L;
 
-    private Token expectedToken;
+    @Value("${jwt.secret}")
+    String secret;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    private UserPrincipal paramPrincipal;
+
+    private UserPrincipal expectedPrincipal;
 
     @BeforeEach
     void setUp() {
+        paramPrincipal = new UserPrincipal("dev teller", "devteller123@gmail.com");
         expectedPrincipal = new UserPrincipal("dev teller", "devteller123@gmail.com");
-        expectedToken = new Token("ACCESS_TOKEN", "REFRESH_TOKEN");
     }
 
     @Test
     @DisplayName("JWT 생성 시, 인증된 사용자 정보를 받으면 인증 정보 클레임이 있는 JWT를 반환한다")
     void user_principal_create_token_test() {
-        // Given
-        UserPrincipal principal = new UserPrincipal("dev teller", "devteller123@gmail.com");
-
         // When
-        String token = jwtProvider.createToken(principal);
+        String token = jwtProvider.createToken(paramPrincipal, EXPIRATION_MILLISECONDS);
         DecodedJWT result = JWT.decode(token);
 
         // Then
-        assertThat(result.getClaim("name").asString()).isEqualTo(expectedPrincipal.getName());
+        assertThat(result.getIssuer()).isEqualTo("dev StoryTeller");
+        assertThat(result.getSubject()).isEqualTo("dev StoryTeller");
+        assertThat(result.getClaim("id").asString()).isEqualTo(expectedPrincipal.getName());
         assertThat(result.getClaim("email").asString()).isEqualTo(expectedPrincipal.getEmail());
     }
 
@@ -45,11 +56,13 @@ public class JwtProviderTest extends ComponentTestEnv {
     @DisplayName("JWT 생성 시, 값을 넘겨주지 않으면 기본 클레임만 있는 JWT를 반환한다")
     void no_param_create_token_test() {
         // When
-        String token = jwtProvider.createToken();
+        String token = jwtProvider.createToken(EXPIRATION_MILLISECONDS);
         DecodedJWT result = JWT.decode(token);
 
         // Then
-        assertNull(result.getClaim("name").asString());
+        assertThat(result.getIssuer()).isEqualTo("dev StoryTeller");
+        assertThat(result.getSubject()).isEqualTo("dev StoryTeller");
+        assertNull(result.getClaim("id").asString());
         assertNull(result.getClaim("email").asString());
     }
 
@@ -57,10 +70,20 @@ public class JwtProviderTest extends ComponentTestEnv {
     @DisplayName("JWT 인증 시, 유효한 JWT를 받으면 사용자 정보를 반환한다")
     void valid_token_create_user_principal_test() {
         // Given
-        Token token = new Token("ACCESS_TOKEN", "REFRESH_TOKEN");
+        Date now = new Date();
+        Date expired = new Date(now.getTime() + EXPIRATION_MILLISECONDS);
+
+        String accessToken = JWT.create()
+                .withIssuer("dev StoryTeller")
+                .withSubject("dev StoryTeller")
+                .withAudience(paramPrincipal.getName())
+                .withClaim("id", paramPrincipal.getName())
+                .withClaim("email", paramPrincipal.getEmail())
+                .withExpiresAt(expired)
+                .sign(Algorithm.HMAC256(secret));
 
         // When
-        UserPrincipal result = jwtProvider.verifyToken(token.accessToken());
+        UserPrincipal result = jwtProvider.verifyToken(accessToken);
 
         // Then
         assertThat(result.getName()).isEqualTo(expectedPrincipal.getName());
@@ -70,18 +93,29 @@ public class JwtProviderTest extends ComponentTestEnv {
     @Test
     @DisplayName("JWT 검증 시, 만료된 JWT를 받으면 TokenExpiredException을 반환한다")
     void expired_token_throw_token_expired_exception() {
-        String refreshToken = "REFRESH_TOKEN";
+        // Given
+        Date now = new Date();
+        Date expired = new Date(now.getTime() + 1L);
+
+        String accessToken = JWT.create()
+                .withIssuer("dev StoryTeller")
+                .withSubject("dev StoryTeller")
+                .withAudience(paramPrincipal.getName())
+                .withClaim("id", paramPrincipal.getName())
+                .withClaim("email", paramPrincipal.getEmail())
+                .withExpiresAt(expired)
+                .sign(Algorithm.HMAC256(secret));
 
         assertThrows(TokenExpiredException.class,
-                () -> jwtProvider.verifyToken(refreshToken));
+                () -> jwtProvider.verifyToken(accessToken));
     }
 
     @Test
     @DisplayName("JWT 검증 시, 잘못된 JWT를 받으면 JWTVerificationException을 반환한다")
     void no_valid_token_throw_jwt_verification_exception() {
-        String refreshToken = "REFRESH_TOKEN";
+        String accessToken = "NO_VALID_ACCESS_TOKEN";
 
         assertThrows(JWTVerificationException.class,
-                () -> jwtProvider.verifyToken(refreshToken));
+                () -> jwtProvider.verifyToken(accessToken));
     }
 }
